9076b415809564875cf81e66c073db4465a988bc
povilaspetkevicius <p.petkevicius200@gmail.com>
2020-04-11 12:09:56 +0300

    Disabling CORS filter

--- src/main/java/com/petkevicius/timer_event/controller/EventController.java
+++ src/main/java/com/petkevicius/timer_event/controller/EventController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/")
 public class EventController {
